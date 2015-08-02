dee4656eae62b13297876b1176f5143d2ba48030
vade <vade@vade.info>
2015-08-02 10:54:12 -0400

    Working 32 bit only image loader - fails for 8 bit images. Need to introspect bpp to set up image provider.

---
+++ v002 Image Loader/v002_Image_LoaderPlugIn.h
@@ -0,0 +1,25 @@
+//
+//  v002_Image_LoaderPlugIn.h
+//  v002 Image Loader
+//
+//  Created by vade on 8/1/15.
+//  Copyright (c) 2015 v002. All rights reserved.
+//
+
+#import <Quartz/Quartz.h>
+
+@interface v002_Image_LoaderPlugIn : QCPlugIn
+{
+    CGImageRef image;
+}
+
+@property (assign) BOOL loadedImage;
+
+@property (copy) NSString* inputImagePath;
+@property (assign) NSInteger inputRenderingIntent; // CGColorRenderingIntent
+@property (assign) BOOL inputColorCorrect; // CGColorRenderingIntent
+
+
+@property (retain) id<QCPlugInOutputImageProvider> outputImage;
+
+@end
---
+++ v002 Image Loader/v002_Image_LoaderPlugIn.m
@@ -0,0 +1,175 @@
+//
+//  v002_Image_LoaderPlugIn.m
+//  v002 Image Loader
+//
+//  Created by vade on 8/1/15.
+//  Copyright (c) 2015 v002. All rights reserved.
+//
+
+// It's highly recommended to use CGL macros instead of changing the current context for plug-ins that perform OpenGL rendering
+#import <OpenGL/CGLMacro.h>
+
+#import "v002_Image_LoaderPlugIn.h"
+
+#define	kQCPlugIn_Name				@"v002 Image Loader"
+#define	kQCPlugIn_Description		@"v002 Image Loader description"
+
+void MyQCPlugInBufferReleaseCallback(const void* address, void * context)
+{
+    CGImageRef image = (CGImageRef) context;
+    CGImageRelease(image);
+}
+
+@implementation v002_Image_LoaderPlugIn
+
+
+@synthesize loadedImage;
+
+@dynamic inputImagePath;
+@dynamic inputRenderingIntent;
+@dynamic inputColorCorrect;
+@dynamic outputImage;
+
++ (NSDictionary *)attributes
+{
+    return @{QCPlugInAttributeNameKey:kQCPlugIn_Name, QCPlugInAttributeDescriptionKey:kQCPlugIn_Description};
+}
+
++ (NSDictionary *)attributesForPropertyPortWithKey:(NSString *)key
+{
+	return nil;
+}
+
++ (QCPlugInExecutionMode)executionMode
+{
+	return kQCPlugInExecutionModeProvider;
+}
+
++ (QCPlugInTimeMode)timeMode
+{
+	return kQCPlugInTimeModeNone;
+}
+
+- (instancetype)init
+{
+	self = [super init];
+	if (self)
+    {
+        image = NULL;
+        self.loadedImage = NO;
+	}
+	
+	return self;
+}
+
+- (void) dealloc
+{
+    [super dealloc];
+
+//    if(image)
+//    {
+//        CGImageRelease(image);
+//        image = NULL;
+//    }
+
+}
+
+@end
+
+@implementation v002_Image_LoaderPlugIn (Execution)
+
+- (BOOL)startExecution:(id <QCPlugInContext>)context
+{
+	return YES;
+}
+
+- (void)enableExecution:(id <QCPlugInContext>)context
+{
+}
+
+- (BOOL)execute:(id <QCPlugInContext>)context atTime:(NSTimeInterval)time withArguments:(NSDictionary *)arguments
+{
+	
+//	CGLContextObj cgl_ctx = [context CGLContextObj];
+	
+    if([self didValueForInputKeyChange:@"inputImagePath"] || [self didValueForInputKeyChange:@"inputColorCorrect"])
+    {
+        // Bail early if we have a nil or empty path because f that noise.
+        if(!self.inputImagePath || (self.inputImagePath.length == 0))
+        {
+            return YES;
+        }
+//        if(image)
+//        {
+//            CGImageRelease(image);
+//            image = NULL;
+//        }
+        
+        // load image
+        CGImageSourceRef imageSource;
+        NSDictionary* opts = @{(NSString*)kCGImageSourceShouldAllowFloat : @YES,
+                               (NSString*)kCGImageSourceShouldCache : @YES,
+                               (NSString*)kCGImageSourceShouldCacheImmediately : @YES};
+        
+        NSString* standardPath = [self.inputImagePath stringByStandardizingPath];
+        
+        NSURL* filePathURL = [NSURL fileURLWithPath:standardPath];
+        imageSource = CGImageSourceCreateWithURL((CFURLRef) filePathURL, NULL);
+        
+        image = CGImageSourceCreateImageAtIndex(imageSource, 0, (CFDictionaryRef)opts);
+    
+        // bad form this is not an image provider but QC will swizzle ?
+
+        id<QCPlugInOutputImageProvider> imageProvider = nil;
+        
+        //            NSString* pixelFormat = nil;
+        //            CGBitmapInfo bitmapInfo = CGImageGetBitmapInfo(image);
+        
+        
+        size_t imageWidth = CGImageGetWidth(image);
+        size_t imageHeight = CGImageGetHeight(image);
+        size_t imageBytesPerRow = CGImageGetBytesPerRow(image);
+        CGColorSpaceRef imageColorSpace = CGImageGetColorSpace(image);
+        
+        CGDataProviderRef imageDataProvider = CGImageGetDataProvider(image);
+        
+        CFDataRef data = CGDataProviderCopyData(imageDataProvider);
+        void* baseAdrress = CFDataGetBytePtr(data);
+        
+        imageProvider = [context outputImageProviderFromBufferWithPixelFormat:QCPlugInPixelFormatRGBAf
+                                                                   pixelsWide:imageWidth
+                                                                   pixelsHigh:imageHeight
+                                                                  baseAddress:baseAdrress
+                                                                  bytesPerRow:imageBytesPerRow
+                                                              releaseCallback:MyQCPlugInBufferReleaseCallback
+                                                               releaseContext:image
+                                                                   colorSpace:imageColorSpace
+                                                             shouldColorMatch:self.inputColorCorrect];
+        
+        CGDataProviderRelease(imageDataProvider);
+        
+        if(imageProvider)
+            self.outputImage = imageProvider;
+        else
+            self.outputImage = nil;
+
+    }
+    
+	return YES;
+}
+
+- (void)disableExecution:(id <QCPlugInContext>)context
+{
+}
+
+- (void)stopExecution:(id <QCPlugInContext>)context
+{
+//    if(image)
+//    {
+//        CGImageRelease(image);
+//        image = NULL;
+//        self.loadedImage = NO;
+//    }
+}
+
+@end
